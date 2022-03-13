933032b6f5dbe6d10bf0ecf127aa6fde1f290f40
Rémi Defoor <remi.defoor@student.howest.be>
2022-03-13 13:33:29 +0100

    create books router skeleton

---
+++ routes/books.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router;
+
+router.route('/')
+    .get((res, req) => {
+
+    })
+    .post((res, req) => {
+
+    });
+
+router.route('/:isbn')
+    .delete((res, req) => {
+
+    });
+
+module.exports = router;
