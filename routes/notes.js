015a5cb21ef50d1eb5a05672bbaa69e84964bc8b
Rémi Defoor <remi.defoor@student.howest.be>
2022-03-13 13:38:18 +0100

    create notes router skeleton

---
+++ routes/notes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router;
+
+router.route('/')
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+
+    });
+
+router.route('/:id')
+    .delete((req, res) => {
+
+    });
+
+module.exports = router;
