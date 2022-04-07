870d6a2c6f677d6d7812b062ec387331dd6ec120
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-07 11:06:47 +0200

    remove trailing new line from logs view

--- src/data/fsLogsRepository.js
+++ src/data/fsLogsRepository.js
@@ -5,6 +5,7 @@ const ACCESS_LOGS_PATH = 'logs/access.log';
 async function readAccessLogs() {
   const accessLogs = await fs.readFile(ACCESS_LOGS_PATH, 'utf-8');
   const accessLogsArray = accessLogs.split('\n');
+  accessLogsArray.pop(); // remove trailing new line
   return accessLogsArray;
 }
 
