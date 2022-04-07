f613dd40cf4e053c4fc160e5f4d332b0b24b754e
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-07 10:47:47 +0200

    create log controller

---
+++ src/controllers/logController.js
@@ -0,0 +1,13 @@
+const logService = require('../services/logService');
+
+async function getAccessLogs(req, res) {
+  res.render('logs', {
+    title: 'Access Logs',
+    logs: await logService.retrieveAccessLogs()
+    }
+  )
+}
+
+module.exports = {
+  getAccessLogs
+}
